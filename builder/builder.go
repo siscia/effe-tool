1a4d4fe099ccab8827bb5972ebda4f4b21581a7b
siscia <simone@mweb.biz>
2016-03-04 19:23:03 +0100

    mainly doc, minor error

--- builder/builder.go
+++ builder/builder.go
@@ -19,11 +19,16 @@ type info struct {
 	Doc     string `json:"doc"`
 }
 
+// randomSuffix return a string to be used to generate random
+// temporany directories.
 func randomSuffix() string {
 	return strconv.Itoa(100000 + rand.Intn(1000000))
 }
 
-func GetNameVersion(path string) (name, version string, err error) {
+// getNameVersion execute the binary with the `-info` option.
+// Then it parse the standard output, parse the JSON and
+// return a name and a version string
+func getNameVersion(path string) (name, version string, err error) {
 	name = ""
 	version = ""
 	cmd := exec.Command(path, "-info", "True")
@@ -50,11 +55,21 @@ func GetNameVersion(path string) (name, version string, err error) {
 	return
 }
 
-func CreateFilenameExecutable(name, version string) string {
+func createFilenameExecutable(name, version string) string {
 	return name + "_v" + version
 }
 
+// compileSingleFile compile an effe to a single binary.
+// It start by creating a temporany directory where it moves
+// the logic of the effe, and the core.
+// Then it adds the temporany dir just created to the GOPATH
+// Finally it invoke the go tool to actually compile the file,
+// it redirects the Stdout and the Stderr so that the user can
+// actually see compilation errors.
+// It returns the path where the executable is been created
 func compileSingleFile(source_path string) (string, error) {
+
+	// Creating temporany directory and structure
 	dir := os.TempDir() + "/effebuild-" + randomSuffix()
 	if err := os.Mkdir(dir, 0777); err != nil {
 		fmt.Println(err)
@@ -83,11 +98,16 @@ func compileSingleFile(source_path string) (string, error) {
 		return "", err
 	}
 
+	// adding temporany dir to the GOPATH
 	gopath := os.Getenv("GOPATH")
 	os.Setenv("GOPATH", dir+":"+gopath)
 	defer os.Setenv("GOPATH", gopath)
 
+	// actually compile
 	cmd := exec.Command("go", "build", "-a", "-o", dir+"/out", "-buildmode=exe", dir_effe+"/effe.go")
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
 	if err := cmd.Start(); err != nil {
 		fmt.Println(err)
@@ -101,42 +121,85 @@ func compileSingleFile(source_path string) (string, error) {
 	return dir + "/out", nil
 }
 
-func compileDirectory(path string, c *cli.Context) {}
-func compileFile(path string, c *cli.Context) error {
-	fmt.Println(*c)
-	fmt.Println(path)
+// compileFile is the entry point to compile an effe source.
+// The actual compilation is done by `compileSingleFile` but
+// `compileFile` takes care of move the binary where the user
+// is expecting.
+// It first compile the file (passed as path).
+// It execute the just compiled file to gather information
+// about name and version.
+// Finally it moves the executable.
+//
+// `path` is where the effe source is located.
+// `dirName` rappresent in which directory save the executable,
+// it has a default value set on the flag to `out`.
+// `execName` is the name of the executable, if not given
+// `compileFile` try to use the effe convetion to provide a name.
+func compileFile(path, dirName, execName string) error {
+	// Actually compiling
 	tmpExecPath, err := compileSingleFile(path)
 	if err != nil {
-		fmt.Println("Impossible to compile: " + path)
+		fmt.Println("File: " + path + " | Impossible to compile.")
 		return err
 	}
-	dirName := c.String("dirout")
-	fmt.Println(c.IsSet("dirout"))
-	execName := c.String("out")
+
+	// Gathering information
 	execVersion := ""
 	if execName == "" {
-		execName, execVersion, err = GetNameVersion(tmpExecPath)
+		execName, execVersion, err = getNameVersion(tmpExecPath)
+		if err != nil {
+			fmt.Println("File: " + path + " | Error in the executable info, actual path is: " + tmpExecPath)
+			return err
+		}
 	}
-	execName = CreateFilenameExecutable(execName, execVersion)
+
+	// Moving the file
+	execName = createFilenameExecutable(execName, execVersion)
 	totalPath, err := filepath.Abs(dirName + `/` + execName)
-	fmt.Println(totalPath)
 	if err != nil {
-		fmt.Println("Error in getting the path.")
+		fmt.Println("File: " + path + " | Error in getting the absolute path, actual path is: " + tmpExecPath)
 		return err
 	}
 	if err := os.MkdirAll(filepath.Dir(totalPath), 0777); err != nil {
-		fmt.Println("Impossible to create the directory: " + filepath.Dir(totalPath))
+		fmt.Println("File: " + path + " | Impossible to create the directory: " + filepath.Dir(totalPath))
 		return err
 	}
 	if err := os.Rename(tmpExecPath, totalPath); err != nil {
 		fmt.Println(err)
-		fmt.Println("Impossible to move the executable, actual path is: " + tmpExecPath)
+		fmt.Println("File: " + path + " | Impossible to move the executable, actual path is: " + tmpExecPath)
 	}
+	fmt.Println("File: " + path + " | Everything went good, the file is been compiled and the executable is on: " + totalPath)
 	return nil
-
 }
 
-func moveExecutable(path string) {}
+// compileDirectory simply walks the filesystem and
+// try to compile every file it find.
+// The real job is done by `walkAndCompile`
+// walkAndCompile simply does nothing to the directory.
+// walkAndCompile preserve the shape of the source dir
+// into the executable directory.
+func compileDirectory(originalPath string, c *cli.Context) {
+	walkAndCompile := func(path string, f os.FileInfo, _ error) error {
+		if f.IsDir() {
+			return nil
+		}
+		if f.Mode().IsRegular() {
+			fmt.Println()
+			relativePath, err := filepath.Rel(originalPath, path)
+			if err != nil {
+				fmt.Println("File: " + path + " | Error with the relative path.")
+				return nil
+			}
+			execLocation := filepath.Dir(relativePath)
+			err = compileFile(path, c.String("dirout")+"/"+execLocation, "")
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		return nil
+	}
+	filepath.Walk(originalPath, walkAndCompile)
+}
 
 func Compile(c *cli.Context) {
 	path := c.Args().First()
@@ -149,7 +212,7 @@ func Compile(c *cli.Context) {
 		compileDirectory(path, c)
 	}
 	if f.Mode().IsRegular() {
-		err := compileFile(path, c)
+		err := compileFile(path, c.String("dirout"), c.String("out"))
 		if err != nil {
 			fmt.Println(err)
 		}
